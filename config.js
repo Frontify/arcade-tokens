e0ae1ab637738518884fe12ef75e4f3b365e1838
thisguyscode <jamio.mitchell@gmail.com>
2021-10-29 11:54:00 +0200

    Populates some tokens and sets build

--- config.js
+++ config.js
@@ -1,22 +1,84 @@
+const inputDirectory = "input";
+const outputDirectory = "output";
+
+// Set Checks
+const isUi = (token) => {
+  return token.filePath.indexOf(`ui.`) > -1;
+};
+const isBrand = (token) => {
+  return token.filePath.indexOf(`brand.`) > -1;
+};
+
+// Category Checks
+const isFontCategory = (token) => {
+  return token.attributes.category === "font";
+};
+const isSizeCategory = (token) => {
+  return token.attributes.category === "size";
+};
+const isColorCategory = (token) => {
+  return token.attributes.category === "color";
+};
+
+// Type Checks
+const isFontType = (token) => {
+  return token.attributes.type === "font";
+};
+const isSpacingType = (token) => {
+  return token.attributes.type === "spacing";
+};
+
+// Combined Checks
+const isFontSize = (token) => {
+  return isSizeCategory(token) && isFontType(token);
+};
+const isTypography = (token) => {
+  return isFontCategory(token) || isFontSize(token);
+};
+const isSpacing = (token) => {
+  return isSizeCategory(token) && isSpacingType(token);
+};
+
 module.exports = {
-  source: ["tokens/**/*.js"],
+  source: [inputDirectory + "/**/*.js"],
   platforms: {
     css: {
       transformGroup: "css",
-      buildPath: "build/css/",
+      buildPath: outputDirectory + "/css/",
       files: [
         {
-          destination: "brand.css",
+          destination: "brand.typography.css",
           format: "css/variables",
           filter: (token) => {
-            return token.filePath.indexOf(`brand.`) > -1;
+            return isTypography(token) && isBrand(token);
           },
         },
         {
-          destination: "ui.css",
+          destination: "ui.typography.css",
           format: "css/variables",
           filter: (token) => {
-            return token.filePath.indexOf(`ui.`) > -1;
+            return isTypography(token) && isUi(token);
+          },
+        },
+        {
+          destination: "brand.colors.css",
+          format: "css/variables",
+          filter: (token) => {
+            return isColorCategory(token) && isBrand(token);
+          },
+        },
+        {
+          destination: "ui.colors.css",
+          format: "css/variables",
+          filter: (token) => {
+            return isColorCategory(token) && isUi(token);
+          },
+        },
+        {
+          destination: "ui.spacing.css",
+          format: "css/variables",
+          filter: (token) => {
+            return isSpacing(token) && isUi(token);
           },
         },
       ],
@@ -26,7 +88,7 @@ module.exports = {
     },
     android: {
       transformGroup: "android",
-      buildPath: "build/android/",
+      buildPath: outputDirectory + "/android/",
       files: [
         {
           destination: "font_dimens.xml",
---
+++ input/ui.typography.js
@@ -0,0 +1,126 @@
+module.exports = {
+  body: {
+    fontFamily: {
+      value: "{brand.font.spaceGrotesk.family.value}",
+      attributes: { category: "font" },
+    },
+    "01": {
+      size: {
+        value: "14",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.spaceGrotesk.regular.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "18",
+        attributes: { category: "size", type: "line-height" },
+      },
+      lineHeightLong: {
+        value: "22",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+    "02": {
+      size: {
+        value: "16",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.spaceGrotesk.regular.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "22",
+        attributes: { category: "size", type: "line-height" },
+      },
+      longLineHeight: {
+        value: "24",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+  },
+  heading: {
+    fontFamily: {
+      value: "{brand.font.spaceGrotesk.family.value}",
+      attributes: { category: "font" },
+    },
+    "01": {
+      size: {
+        value: "14",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.spaceGrotesk.semiBold.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "18",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+    "02": {
+      size: {
+        value: "16",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.spaceGrotesk.semiBold.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "22",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+    "03": {
+      size: {
+        value: "24",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.spaceGrotesk.semiBold.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "28",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+  },
+  code: {
+    fontFamily: {
+      value: "{brand.font.menlo.family.value}",
+      attributes: { category: "font" },
+    },
+    "01": {
+      size: {
+        value: "14",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.menlo.regular.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "18",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+    "02": {
+      size: {
+        value: "16",
+        attributes: { category: "size", type: "font" },
+      },
+      fontWeight: {
+        value: "{brand.font.menlo.regular.weight.value}",
+        attributes: { category: "font", type: "weight" },
+      },
+      lineHeight: {
+        value: "22",
+        attributes: { category: "size", type: "line-height" },
+      },
+    },
+  },
+};
