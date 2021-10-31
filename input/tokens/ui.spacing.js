8a403c3ef1336a9e22cf6e5563d95f6bd8da78d2
thisguyscode <jamio.mitchell@gmail.com>
2021-10-31 19:11:02 +0100

    Whitespace cleanup

--- input/tokens/ui.spacing.js
+++ input/tokens/ui.spacing.js
@@ -1,27 +1,12 @@
 module.exports = {
   space: {
-    xxxSmall: {
-      value: 2,
-      attributes: { category: "size", type: "spacing" },
-    },
-    xxSmall: {
-      value: 4,
-      attributes: { category: "size", type: "spacing" },
-    },
+    xxxSmall: { value: 2, attributes: { category: "size", type: "spacing" } },
+    xxSmall: { value: 4, attributes: { category: "size", type: "spacing" } },
     xSmall: { value: 8, attributes: { category: "size", type: "spacing" } },
     small: { value: 12, attributes: { category: "size", type: "spacing" } },
-    medium: {
-      value: 16,
-      attributes: { category: "size", type: "spacing" },
-    },
+    medium: { value: 16, attributes: { category: "size", type: "spacing" } },
     large: { value: 24, attributes: { category: "size", type: "spacing" } },
-    xLarge: {
-      value: 32,
-      attributes: { category: "size", type: "spacing" },
-    },
-    xxLarge: {
-      value: 40,
-      attributes: { category: "size", type: "spacing" },
-    },
+    xLarge: { value: 32, attributes: { category: "size", type: "spacing" } },
+    xxLarge: { value: 40, attributes: { category: "size", type: "spacing" } },
   },
 };
