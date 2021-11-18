b975b5de635fa9316434b65914c25ecf0c8d62e4
thisguyscode <jamio.mitchell@gmail.com>
2021-11-18 21:39:52 +0100

    Adds attributes to elements for build

--- tokens/ui.elements.js
+++ tokens/ui.elements.js
@@ -5,59 +5,59 @@
 module.exports = {
   // General
   base: {
-    color: { value: "{color.grey.0.value}" },
+    color: { value: "{color.neutral.0.value}", attributes: { category: 'color' } },
   },
   text: {
     color: {
-      _    : { value: "{color.neutral.80.value}" },
-      weak : { value: "{color.neutral.60.value}" },
+      _    : { value: "{color.neutral.80.value}", attributes: { category: 'color' } },
+      weak : { value: "{color.neutral.60.value}", attributes: { category: 'color' } },
     },
   },
   line: {
     width: {
-      _      : { value: "1px" },
-      strong : { value: "2px" },
+      _      : { value: "1px", attributes: { category: 'size', type: 'lineWidth' } },
+      strong : { value: "2px", attributes: { category: 'size', type: 'lineWidth' } },
     },
   },
   border: {
     color: {
-      _       : { value: "{color.neutral.20.value}" },
-      strong  : { value: "{color.neutral.30.value}" },
-      xStrong : { value: "{color.neutral.80.value}" },
+      _       : { value: "{color.neutral.20.value}", attributes: { category: 'color' } },
+      strong  : { value: "{color.neutral.30.value}", attributes: { category: 'color' } },
+      xStrong : { value: "{color.neutral.80.value}", attributes: { category: 'color' } },
     },
     width: {
-      _      : { value: "{line.width._.value}" },
-      strong : { value: "{line.width.strong.value}" },
+      _      : { value: "{line.width._.value}", attributes: { category: 'size', type: 'lineWidth' } },
+      strong : { value: "{line.width.strong.value}", attributes: { category: 'size', type: 'lineWidth' } },
     },
     radius: {
-      _      : { value: "4px" },
-      small  : { value: "2px" },
-      large  : { value: "8px" },
-      xLarge : { value: "12px" },
+      _      : { value: "4px", attributes: { category: 'size', type: 'borderRadius' } },
+      small  : { value: "2px", attributes: { category: 'size', type: 'borderRadius' } },
+      large  : { value: "8px", attributes: { category: 'size', type: 'borderRadius' } },
+      xLarge : { value: "12px", attributes: { category: 'size', type: 'borderRadius' } },
     },
   },
   shadow: {
-    _          : { value: "0 3px 10px 0 {shadow.color.value}" },
-    top        : { value: "0 -10px 10px -5px {shadow.color.value}" },
-    bottom     : { value: "0 10px 10px -5px {shadow.color.value}" },
-    color      : {
-      value      : "{color.darkest.value}",
-      attributes : { category: 'color' },
-      modify     : [{ type: "alpha", amount: 0.1}]
+    _               : { value: "0 3px 10px 0 {shadow.color.value}" },
+    top             : { value: "0 -10px 10px -5px {shadow.color.value}" },
+    bottom          : { value: "0 10px 10px -5px {shadow.color.value}" },
+    color: {
+      value         : "{color.darkest.value}",
+      attributes    : { category: 'color' },
+      modify        : [{ type: "alpha", amount: 0.1}]
     },
-    large      : {
-      _          : { value: "0 25px 80px 0 {shadow.large.color.value}" },
-      color      : {
-        value      : "{color.darkest.value}",
-        attributes : { category: 'color' },
-        modify     : [{ type: "alpha", amount: 0.4 }]
+    large: {
+      _             : { value: "0 25px 80px 0 {shadow.large.color.value}" },
+      color: {
+        value       : "{color.darkest.value}",
+        attributes  : { category: 'color' },
+        modify      : [{ type: "alpha", amount: 0.4 }]
       },
     },
   },
   // Components
   checkbox: {
     border: {
-      width: { value: "{border.width.strong.value}" },
+      width: { value: "{border.width.strong.value}", attributes: { category: 'size', type: 'lineWidth' } },
     },
   },
   button: {
@@ -67,10 +67,10 @@ module.exports = {
   },
   input: {
     border: {
-      color: { value: "{border.color._.value}", attributes: { category: "color" }  },
+      color: { value: "{border.color._.value}", attributes: { category: "color" } },
     },
     label: {
-      color: { value: "{text.color._.value}", attributes: { category: "color" }  },
+      color: { value: "{text.color._.value}", attributes: { category: "color" } },
     },
   },
 };
