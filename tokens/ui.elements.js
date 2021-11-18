b354221b80a63afa7dca8759cb357297d2626d64
thisguyscode <jamio.mitchell@gmail.com>
2021-11-18 15:52:00 +0100

    Refactors brand/ui colors + elements

--- tokens/ui.elements.js
+++ tokens/ui.elements.js
@@ -6,73 +6,55 @@ module.exports = {
   // General
   text: {
     color: {
-      _: { value: "{text.color.50.value}" },
-      10: { value: "{color.neutral.10.value}" },
-      20: { value: "{color.neutral.120.value}" },
-      30: { value: "{color.neutral.130.value}" },
-      40: { value: "{color.neutral.140.value}" },
-      50: { value: "{color.neutral.150.value}" },
+      _    : { value: "{color.neutral.80.value}" },
+      weak : { value: "{color.neutral.60.value}" },
     },
   },
   line: {
     width: {
-      _: { value: "{line.width.1.value}" },
-      1: { value: "1px" },
-      2: { value: "2px" },
-      3: { value: "4px" },
+      _      : { value: "1px" },
+      strong : { value: "2px" },
     },
   },
   border: {
     color: {
-      _: { value: "{border.color.30.value}" },
-      10: { value: "{color.neutral.20.value}" },
-      20: { value: "{color.neutral.40.value}" },
-      30: { value: "{color.neutral.60.value}" },
-      40: { value: "{color.neutral.80.value}" },
+      _       : { value: "{color.neutral.20.value}" },
+      strong  : { value: "{color.neutral.30.value}" },
+      xStrong : { value: "{color.neutral.80.value}" },
     },
     width: {
-      _: { value: "{border.width.1.value}" },
-      1: { value: "{line.width.1.value}" },
-      2: { value: "{line.width.2.value}" },
-      3: { value: "{line.width.3.value}" },
+      _      : { value: "{line.width._.value}" },
+      strong : { value: "{line.width.strong.value}" },
     },
     radius: {
-      _: { value: "{border.radius.1.value}" },
-      1: { value: "2px" },
-      2: { value: "4px" },
-      3: { value: "8px" },
+      _      : { value: "4px" },
+      small  : { value: "2px" },
+      large  : { value: "8px" },
+      xLarge : { value: "12px" },
     },
   },
   shadow: {
-    _: { value: "{shadow.medium._.value}" },
-    large: {
-      _: { value: "0 25px 80px 0 {shadow.large.color.value}" },
-      color: {
-        value: "{color.neutral.150.value}",
-        attributes: { category: 'color' },
-        modify: [{ type: "alpha", amount: 0.4 }]
-      },
+    _          : { value: "0 3px 10px 0 {shadow.color.value}" },
+    top        : { value: "0 -10px 10px -5px {shadow.color.value}" },
+    bottom     : { value: "0 10px 10px -5px {shadow.color.value}" },
+    color      : {
+      value      : "{color.darkest.value}",
+      attributes : { category: 'color' },
+      modify     : [{ type: "alpha", amount: 0.1}]
     },
-    medium: {
-      _: { value: "0 3px 10px 0 {shadow.medium.color.value}" },
-      top: { value: "0 -10px 10px -5px {shadow.medium.color.value}" },
-      bottom: { value: "0 10px 10px -5px {shadow.medium.color.value}" },
-      color: {
-        value: "{color.neutral.150.value}",
-        attributes: { category: 'color' },
-        modify: [{ type: "alpha", amount: 0.1}]
+    large      : {
+      _          : { value: "0 25px 80px 0 {shadow.large.color.value}" },
+      color      : {
+        value      : "{color.darkest.value}",
+        attributes : { category: 'color' },
+        modify     : [{ type: "alpha", amount: 0.4 }]
       },
     },
   },
   // Components
-  tooltip: {
-    background: {
-      color: { value: "{color.inverse._.value}" },
-    },
-  },
   checkbox: {
     border: {
-      width: { value: "{border.width.2.value}" },
+      width: { value: "{border.width.strong.value}" },
     },
   },
   button: {
