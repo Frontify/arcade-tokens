31e702a0c4cdf7c7646d8c923f3878196456b06f
thisguyscode <jamio.mitchell@gmail.com>
2021-11-23 16:39:42 +0100

    Adjusts dark theme highlights

--- tokens/ui.theme.dark.js
+++ tokens/ui.theme.dark.js
@@ -30,20 +30,20 @@ module.exports = {
 
   highlight: {
     // Default
-    "color"                     : { value: "{color.neutral-90.value}", attributes: { category: "color" } },
+    "color"                     : { value: "{color.neutral-70.value}", attributes: { category: "color" } },
     "color-inverse"             : { value: "{text.color.value}", attributes: { category: "color" } },
     // Negative
-    "negative-color"            : { value: "{color.negative-80.value}", attributes: { category: "color" } },
-    "negative-color-inverse"    : { value: "{color.negative-10.value}", attributes: { category: "color" } },
+    "negative-color"            : { value: "{color.negative-100.value}", attributes: { category: "color" } },
+    "negative-color-inverse"    : { value: "{color.negative-30.value}", attributes: { category: "color" } },
     // Positive
-    "positive-color"            : { value: "{color.positive-90.value}", attributes: { category: "color" } },
-    "positive-color-inverse"    : { value: "{color.positive-10.value}", attributes: { category: "color" } },
+    "positive-color"            : { value: "{color.positive-100.value}", attributes: { category: "color" } },
+    "positive-color-inverse"    : { value: "{color.positive-30.value}", attributes: { category: "color" } },
     // Warning
-    "warning-color"             : { value: "{color.warning-90.value}", attributes: { category: "color" } },
-    "warning-color-inverse"     : { value: "{color.warning-10.value}", attributes: { category: "color" } },
+    "warning-color"             : { value: "{color.warning-100.value}", attributes: { category: "color" } },
+    "warning-color-inverse"     : { value: "{color.warning-30.value}", attributes: { category: "color" } },
     // Informative
-    "interactive-color"         : { value: "{color.interactive-80.value}", attributes: { category: "color" } },
-    "interactive-color-inverse" : { value: "{color.interactive-10.value}", attributes: { category: "color" } },
+    "interactive-color"         : { value: "{color.interactive-100.value}", attributes: { category: "color" } },
+    "interactive-color-inverse" : { value: "{color.interactive-30.value}", attributes: { category: "color" } },
      // Strong
     "strong-color"              : { value: "{color.neutral-0.value}", attributes: { category: "color" } },
     "strong-color-inverse"      : { value: "{color.neutral-80.value}", attributes: { category: "color" } },
