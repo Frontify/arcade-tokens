d215a4a96604231200de581363194d27f7bc2534
thisguyscode <jamio.mitchell@gmail.com>
2021-12-01 08:31:36 +0100

    Reorders some color tokens

--- tokens/alias.text.js
+++ tokens/alias.text.js
@@ -5,9 +5,9 @@
 
 module.exports = {
   text: {
-    "color-x-weak"      : { value: "{color.grey.50.value}", attributes: { category: "color" } },
-    "color-weak"        : { value: "{color.grey.60.value}", attributes: { category: "color" } },
     "color"             : { value: "{color.grey.80.value}", attributes: { category: "color" } },
+    "color-weak"        : { value: "{color.grey.60.value}", attributes: { category: "color" } },
+    "color-x-weak"      : { value: "{color.grey.50.value}", attributes: { category: "color" } },
     "color-negative"    : { value: "{color.red.70.value}", attributes: { category: "color" } },
     "color-positive"    : { value: "{color.green.70.value}", attributes: { category: "color" } },
     "color-warning"     : { value: "{color.yellow.70.value}", attributes: { category: "color" } },
