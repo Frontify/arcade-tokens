cf941d4edec5309e99cae89e54abd926de6483ed
thisguyscode <jamio.mitchell@gmail.com>
2021-12-01 08:56:08 +0100

    Updates disabled colors

--- tokens/alias.disabled.js
+++ tokens/alias.disabled.js
@@ -5,10 +5,11 @@
 
 module.exports = { 
   disabled: {
-    "background-color"         : { value: "{color.grey.10.value}", attributes: { category: "color" } },
-    "background-color-hover"   : { value: "{color.grey.20.value}", attributes: { category: "color" } },
-    "background-color-pressed" : { value: "{color.grey.30.value}", attributes: { category: "color" } },
-    "inverse-color"            : { value: "{color.grey.80.value}", attributes: { category: "color" } },
-    "border-color"             : { value: "{line.color-weak.value}", attributes: { category: "color" } },
+    "background-color"                : { value: "{color.grey.10.value}", attributes: { category: "color" } },
+    "inverse-color"                   : { value: "{text.color-disabled.value}", attributes: { category: "color" } },
+    "border-color"                    : { value: "{line.color-weak.value}", attributes: { category: "color" } },
+    "strong-background-color"         : { value: "{color.grey.30.value}", attributes: { category: "color" } },
+    "strong-inverse-color"            : { value: "{color.grey.0.value}", attributes: { category: "color" } },
+    "strong-border-color"             : { value: "{line.color-weak.value}", attributes: { category: "color" } },
   },
 };
