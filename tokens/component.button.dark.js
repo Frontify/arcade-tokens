90a2ead9efdf5a8f04ff92a1ebc3d13c3c5b83d2
thisguyscode <jamio.mitchell@gmail.com>
2021-12-01 12:26:17 +0100

    Fixed button dark colors

--- tokens/component.button.dark.js
+++ tokens/component.button.dark.js
@@ -5,11 +5,11 @@
 module.exports = { 
   button: {
       // Default Style
-    "background-color"                         : { value: "{color.grey.10.value}", attributes: { category: "color" } },
-    "background-color-hover"                   : { value: "{color.grey.20.value}", attributes: { category: "color" } },
-    "background-color-pressed"                 : { value: "{color.grey.30.value}", attributes: { category: "color" } },
-    "text-color"                               : { value: "{color.grey.80.value}", attributes: { category: "color" } },
-    "icon-color"                               : { value: "{color.grey.80.value}", attributes: { category: "color" } },
+    "background-color"                         : { value: "{color.grey.70.value}", attributes: { category: "color" } },
+    "background-color-hover"                   : { value: "{color.grey.90.value}", attributes: { category: "color" } },
+    "background-color-pressed"                 : { value: "{color.grey.100.value}", attributes: { category: "color" } },
+    "text-color"                               : { value: "{color.grey.0.value}", attributes: { category: "color" } },
+    "icon-color"                               : { value: "{color.grey.0.value}", attributes: { category: "color" } },
     "border-color"                             : { value: "{line.color-weak.value}", attributes: { category: "color" } },
 
     "negative-background-color"                : { value: "{button.background-color.value}", attributes: { category: "color" } },
@@ -27,25 +27,25 @@ module.exports = {
     "positive-border-color"                    : { value: "{line.color-weak.value}", attributes: { category: "color" } },
       
       // Strong Style
-    "strong-background-color"                  : { value: "{color.grey.70.value}", attributes: { category: "color" } },
-    "strong-background-color-hover"            : { value: "{color.grey.90.value}", attributes: { category: "color" } },
-    "strong-background-color-pressed"          : { value: "{color.grey.100.value}", attributes: { category: "color" } },
-    "strong-text-color"                        : { value: "{color.grey.0.value}", attributes: { category: "color" } },
-    "strong-icon-color"                        : { value: "{color.grey.0.value}", attributes: { category: "color" } },
+    "strong-background-color"                  : { value: "{color.grey.0.value}", attributes: { category: "color" } },
+    "strong-background-color-hover"            : { value: "{color.grey.10.value}", attributes: { category: "color" } },
+    "strong-background-color-pressed"          : { value: "{color.grey.20.value}", attributes: { category: "color" } },
+    "strong-text-color"                        : { value: "{color.grey.80.value}", attributes: { category: "color" } },
+    "strong-icon-color"                        : { value: "{color.grey.80.value}", attributes: { category: "color" } },
     "strong-border-color"                      : { value: "{line.color-weak.value}", attributes: { category: "color" } },
 
     "strong-negative-background-color"         : { value: "{button.strong-background-color.value}", attributes: { category: "color" } },
     "strong-negative-background-color-hover"   : { value: "{button.strong-background-color-hover.value}", attributes: { category: "color" } },
     "strong-negative-background-color-pressed" : { value: "{button.strong-background-color-pressed.value}", attributes: { category: "color" } },
     "strong-negative-text-color"               : { value: "{button.strong-text-color.value}", attributes: { category: "color" } },
-    "strong-negative-icon-color"               : { value: "{color.red.50.value}", attributes: { category: "color" } },
+    "strong-negative-icon-color"               : { value: "{color.red.60.value}", attributes: { category: "color" } },
     "strong-negative-border-color"             : { value: "{line.color-weak.value}", attributes: { category: "color" } },
 
     "strong-positive-background-color"         : { value: "{button.strong-background-color.value}", attributes: { category: "color" } },
     "strong-positive-background-color-hover"   : { value: "{button.strong-background-color-hover.value}", attributes: { category: "color" } },
     "strong-positive-background-color-pressed" : { value: "{button.strong-background-color-pressed.value}", attributes: { category: "color" } },
     "strong-positive-text-color"               : { value: "{button.strong-text-color.value}", attributes: { category: "color" } },
-    "strong-positive-icon-color"               : { value: "{color.green.50.value}", attributes: { category: "color" } },
+    "strong-positive-icon-color"               : { value: "{color.green.60.value}", attributes: { category: "color" } },
     "strong-positive-border-color"             : { value: "{line.color-weak.value}", attributes: { category: "color" } },
       
     // Danger Style
