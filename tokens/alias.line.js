a54e3de829966456519c41ddaec1e8dabb073c7c
thisguyscode <jamio.mitchell@gmail.com>
2021-12-08 10:08:22 +0100

    feat: add box alias (#40)
    * Changes line-weak value
    
    * Introduces box alias

--- tokens/alias.line.js
+++ tokens/alias.line.js
@@ -7,7 +7,7 @@ module.exports = {
   line: {
     // Color
     "color"           : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.1 }] },
-    "color-weak"      : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.035 }] },
+    "color-weak"      : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.03 }] },
     "color-strong"    : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.15 }] },
     "color-x-strong"  : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.3 }] },
     "color-xx-strong" : { value: "{color.grey.80.value}", attributes: { category: "color" } },
