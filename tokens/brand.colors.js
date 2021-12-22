5800165cf22e29689bd10646a15f8faa814b6803
thisguyscode <jamio.mitchell@gmail.com>
2021-12-22 13:44:39 +0100

    feat: Changes color.grey.30

--- tokens/brand.colors.js
+++ tokens/brand.colors.js
@@ -15,7 +15,7 @@ module.exports = {
       "5"         : { value: "{color.off-white.value}"},
       "10"        : { value: "#f1f1f1"},
       "20"        : { value: "#e8e9e9"},
-      "30"        : { value: "#c0c1c1"},
+      "30"        : { value: "#D5D6D6"},
       "40"        : { value: "#a5a8a8"},
       "50"        : { value: "#8d9090"},
       "60"        : { value: "#6c7070"},
