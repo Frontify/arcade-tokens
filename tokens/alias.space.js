501e02cd9ccca5a2b8bb32125f6eaf2713862906
Robert Vogt <robert@smartive.ch>
2021-12-23 13:22:33 +0100

    fix: Rename Space Alias Tokens to T-Shirt Sizing

--- tokens/alias.space.js
+++ tokens/alias.space.js
@@ -5,13 +5,13 @@
 
 module.exports = {
     space: {
-      "xx-small" : { value: 0.25, attributes: { category: "size", type: "spacing" } },
-      "x-small"  : { value: 0.5, attributes: { category: "size", type: "spacing" } },
-      "small"    : { value: 0.75, attributes: { category: "size", type: "spacing" } },
-      "medium"   : { value: 1, attributes: { category: "size", type: "spacing" } },
-      "large"    : { value: 1.5, attributes: { category: "size", type: "spacing" } },
-      "x-large"  : { value: 2, attributes: { category: "size", type: "spacing" } },
-      "xx-large" : { value: 2.5, attributes: { category: "size", type: "spacing" } },
+      "xxs" : { value: 0.25, attributes: { category: "size", type: "spacing" } },
+      "xs"  : { value: 0.5, attributes: { category: "size", type: "spacing" } },
+      "s"   : { value: 0.75, attributes: { category: "size", type: "spacing" } },
+      "m"   : { value: 1, attributes: { category: "size", type: "spacing" } },
+      "l"   : { value: 1.5, attributes: { category: "size", type: "spacing" } },
+      "xl"  : { value: 2, attributes: { category: "size", type: "spacing" } },
+      "xxl" : { value: 2.5, attributes: { category: "size", type: "spacing" } },
     },
   };
   
