7796e921e2a25a995822e451096a49a325b1c07b
julianiff <julian.iff@frontify.com>
2022-07-27 11:44:48 +0200

    chore: remove tokens that are not used

--- tokens/alias.base.dark.js
+++ tokens/alias.base.dark.js
@@ -6,8 +6,6 @@
 module.exports = {
   base : {
     "color"     : { value: "{color.grey.80.value}", attributes: { category: "color" } },
-    "color-alt" : { value: "{color.grey.90.value}", attributes: { category: "color" } },
-    "color-transparent" : { value: "transparent", attributes: { category: "color" } },
-    "color-current" : { value: "currentColor", attributes: { category: "color" } },
+    "color-alt" : { value: "{color.grey.90.value}", attributes: { category: "color" } }
   }
 };
--- tokens/alias.base.js
+++ tokens/alias.base.js
@@ -6,8 +6,6 @@
 module.exports = {
   base : {
     "color"     : { value: "{color.grey.0.value}", attributes: { category: "color" } },
-    "color-alt" : { value: "{color.grey.5.value}", attributes: { category: "color" } },
-    "color-transparent" : { value: "transparent", attributes: { category: "color" } },
-    "color-current" : { value: "currentColor", attributes: { category: "color" } },
+    "color-alt" : { value: "{color.grey.5.value}", attributes: { category: "color" } }
   }
 };
