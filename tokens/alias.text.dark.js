29ee92809eaa8ae243c559e3ae6d5fa056f4d60e
James Mitchell <jamio.mitchell@gmail.com>
2023-04-04 15:21:56 +0200

    feat: make dark theme text lighter

--- tokens/alias.text.dark.js
+++ tokens/alias.text.dark.js
@@ -6,8 +6,8 @@
 module.exports = {
   text: {
     "color"             : { value: "{color.grey.0.value}", attributes: { category: "color" } },
-    "color-weak"        : { value: "{color.grey.20.value}", attributes: { category: "color" } },
-    "color-x-weak"      : { value: "{color.grey.40.value}", attributes: { category: "color" } },
+    "color-weak"        : { value: "{color.grey.40.value}", attributes: { category: "color" } },
+    "color-x-weak"      : { value: "{color.grey.60.value}", attributes: { category: "color" } },
     "color-negative"    : { value: "{color.red.40.value}", attributes: { category: "color" } },
     "color-positive"    : { value: "{color.green.40.value}", attributes: { category: "color" } },
     "color-warning"     : { value: "{color.yellow.50.value}", attributes: { category: "color" } },
