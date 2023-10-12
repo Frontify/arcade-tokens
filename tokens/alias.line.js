9be3d88b072455ac360e5747dbadbc43d2b93f27
imoutaharik <ilham.moutaharik@frontify.com>
2023-10-12 12:24:06 +0200

    feat: Add mighty tokens

--- tokens/alias.line.js
+++ tokens/alias.line.js
@@ -11,6 +11,7 @@ module.exports = {
     "color-strong"    : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.15 }] },
     "color-x-strong"  : { value: "{color.grey.100.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.3 }] },
     "color-xx-strong" : { value: "{color.grey.80.value}", attributes: { category: "color" } },
+    "color-mighty" : { value: "{color.grey.0.value}", attributes: { category: "color" }, modify: [{ type: "alpha", amount: 0.15 }] },
     // Width
     "width"           : { value: 0.063, attributes: { category: "size", type: "lineWidth" } },
     "width-large"     : { value: 0.125, attributes: { category: "size", type: "lineWidth" } },
